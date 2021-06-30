067b1837c71d81207021f43b3918a9a77f9d8c97
sseepun <sarun_sla@hotmail.com>
2021-06-30 20:47:24 +0700

    Remove theme folder

--- theme/include/header.php
+++
@@ -1,5 +0,0 @@
-<meta charset="UTF-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<title>UpDtiven</title>
-<link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
--- theme/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-    foreach(scandir('./') as $file){
-        if(strpos($file, '.php')!==false && !in_array($file, ['index.php', 'grids.php'])){
-            echo '<a href="'.$file.'" target="_blank"><h2>'.$file.'</h2></a>';
-        }
-    }
-?>
